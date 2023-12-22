92e0d1fde92221b2f0cf5c6b87beb2d943f06ef6
AhmetNoyan <ahmetnoyan44@gmail.com>
2023-12-22 16:26:14 -0500

    committed

--- src/test/java/utilities/BrowserUtilities.java
+++ src/test/java/utilities/BrowserUtilities.java
@@ -2,12 +2,22 @@ package utilities;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BrowserUtilities {
 
 	Actions action;
+	WebDriverWait wait;
 	public void actionsSendKeys(WebElement element,String text) {
 		action=new Actions(Driver.getDriver());
 		action.sendKeys(element,text).build().perform();
 	}
+	
+	public void waitUntilElementVisible(WebElement element) {
+		wait=new WebDriverWait(Driver.getDriver(),5);
+		wait.until(ExpectedConditions.visibilityOf(element));
+		
+	}
+	
 }
