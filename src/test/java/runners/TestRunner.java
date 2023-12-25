b354b229ff0c25a09125f3c63f458f2e786528d8
AhmetNoyan <ahmetnoyan44@gmail.com>
2023-12-25 01:02:50 -0500

    report file added

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,9 +7,13 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+		plugin ={"pretty", 
+				"html:test_results/cucumber_report.html",
+				"json:test_results/cucumber_report.json"},
 		features="./src/test/resources/features",
 		glue="step_definitions",
 		tags="@validLoginAttempts",
+		publish=true,
 		dryRun=false //It will scan the feature files and it will check if anything is not implemented. It wont run anything.
 		)
 
